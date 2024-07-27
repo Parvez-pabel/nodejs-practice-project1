606006155bf13fa013aec9bfb56daf884b58aa9e
parvez <pabelalomparvez2233@gmail.com>
2024-07-27 20:57:04 +0600

    post api er kaj kora hoyeche

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser  = require('body-parser');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const users = ["Parvez", "Alom", "Samia", "Sadia", "Rohit", "Tuku", "Abid"]; 
+
+app.get('/users/:id', function (req, res) {
+  const id = req.params.id; // params find that is there any parameter which is from website url 
+  const name = users[id];
+  res.send({id, name});
+})
+//post API
+app.post('/addUser', (req, res) =>{
+  res.send(req.body);
 })
 
 app.listen(3000, () =>{
